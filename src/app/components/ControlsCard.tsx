e71044e4ae946cacbfeb67831f2ecb099591f85d
Serjo <serjobarron@gmail.com>
2026-06-08 14:12:47 -0700

    added investigation controls content (system read only instructions & textarea for optional instructions; will adjust instructions as see fit)

--- src/app/components/ControlsCard.tsx
+++ src/app/components/ControlsCard.tsx
@@ -7,7 +7,43 @@ export default function ControlsCard({}) {
       <div className="flex flex-row h-15.5 px-6 justify-between items-center rounded-t-2xl border-b-2 bg-(--bg3) border-(--border)">
         <h1>Investigation Controls</h1> <PanelRight />
       </div>
-      <div className="flex h-full p-6 rounded-b-2xl bg-(--bg2)"> content </div>
+      {/* Content */}
+      <div className="flex flex-col h-full p-6 gap-3 rounded-b-2xl bg-(--bg2)">
+        {/* System Instructions */}
+        <h2>System Instructions (read-only)</h2>
+
+        <hr className="border-(--border)" />
+
+        <div className="flex w-full p-1 text-sm rounded-lg border bg-(--card-bg) border-(--border2)">
+          <div className="flex w-full p-3 rounded-md border bg-(--card-bg2) border-(--border3)">
+            <p>
+              - Extract full legal names including middle names where available
+              (e.g. &quot;Hadden Irving Clark&quot; not &quot;Hadden
+              Clark&quot;)
+              <br />
+              - Treat extracted names as potentially noisy if source is a
+              speech-to-text transcript
+              <br />
+              - Treat location, year, crime type, and keywords as the most
+              reliable signals
+              <br />- For defendant and victim, always prefer the most complete
+              name available
+            </p>
+          </div>
+        </div>
+
+        {/* Your Instructions */}
+        <h2>Your Instructions (optional)</h2>
+
+        <hr className="border-(--border)" />
+
+        <div className="flex w-full p-1 text-sm rounded-lg border bg-(--card-bg) border-(--border2)">
+          <textarea
+            placeholder="Add context to refine the case analysis..."
+            className="resize-none w-full min-h-40 p-3 rounded-md border bg(--bg2) border-(--border3) focus:outline-(--border2) shadow-[inset_0_0_32px_rgba(0,0,0,0.25)]"
+          />
+        </div>
+      </div>
     </div>
   );
 }
