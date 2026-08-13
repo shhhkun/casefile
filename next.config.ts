df54a55be35b71a0f3dfa8e12495e32d4b020270
Serjo <serjobarron@gmail.com>
2026-08-13 16:13:38 -0700

    testing vercel serverless config (avoid C++ env and fallback to wasm)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["onnxruntime-node"],
+  serverExternalPackages: ["@huggingface/transformers"],
 };
 
 export default nextConfig;
